63f09883f99c5dde6438002b984f041934cb428e
hks9999 <hks9999@naver.com>
2021-04-28 16:57:03 +0900

    test

---
+++ init.sh
@@ -0,0 +1,7 @@
+timedatectl set-timezone 'Asia/Seoul'
+apt update 
+apt install nmap -y
+apt install whois -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7 -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
