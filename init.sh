d1add0e290e29209ad2d0f944424cdfceff67569
hks9999 <hks9999@naver.com>
2021-04-28 17:00:08 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 timedatectl set-timezone 'Asia/Seoul'
-apt update 
+apt update
+apt dist-upgrade
 apt install nmap -y
 apt install whois -y
+apt install tor -y
+apt install sqlmap -y
+echo "Google Chrome" 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7 -y
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo "Install Ended"
