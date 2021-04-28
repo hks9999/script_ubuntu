d9f3b85425dc4e7858d63fe50717fbf4578c8d86
hks9999 <hks9999@naver.com>
2021-04-28 17:30:20 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,13 @@ apt install nmap -y
 apt install whois -y
 apt install tor -y
 apt install sqlmap -y
+apt install dict -y 
+
 echo "Google Chrome" 
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7 -y
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
+
 echo "Install Ended"
